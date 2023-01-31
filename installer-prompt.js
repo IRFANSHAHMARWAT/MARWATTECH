dfeab06087eee814d086b9be2c9d92d50bf09f5f
IRFAN SHAH <55048890+IRFANSHAHMARWAT@users.noreply.github.com>
2023-01-31 17:01:58 +0500

    installler prompt for pwa
    installler prompt for pwa by marwat tech

---
+++ installer-prompt.js
@@ -0,0 +1,21 @@
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPrompt();
+});
+
+function showInstallPrompt() {
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
+}
